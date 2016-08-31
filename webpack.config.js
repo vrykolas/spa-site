fc0e4f638632baae71c8e979a4e2cf4be8d35815
Scott Timmins <scott@powershift.tv>
2016-08-31 12:15:10 +0100

    feat: add build tools

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const Webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './src/index.html',
+    './src/js/app.js',
+    './src/sass/style.scss'
+  ],
+  output: {
+    path: './public',
+    filename: 'app.js',
+    publicPath: '/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.html$/,
+        loader: 'file?name=[name].[ext]!html-minify'
+      },
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        loader: 'babel?presets=es2015&cacheDirectory!eslint'
+      },
+      {
+        test: /\.scss$/,
+        loader: 'file?name=style/[name].css!sass'
+      },
+      {
+        test: /\.hbs$/,
+        loader: 'handlebars-loader'
+      }
+    ]
+  }
+};
