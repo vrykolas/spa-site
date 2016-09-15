ba4f62f3845a5c516e2c281ce72bf27d75a5bef6
Scott Timmins <scott@powershift.tv>
2016-09-15 09:38:10 +0100

    feat: add initialization routine

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,7 @@
+import init from './init';
 import Marionette from 'backbone.marionette';
+
+init();
 
 export default Marionette.Application.extend({
   region: '#app',
---
+++ src/js/init.js
@@ -0,0 +1,9 @@
+import window from 'window';
+
+function init() {
+  // Initialize class structure
+  window.App = {};
+  window.App.Views = {};
+}
+
+export default init;
