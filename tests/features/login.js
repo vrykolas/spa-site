5ab0a5ee72d8fb5b806a5f81db774fa0ec26fd38
Scott Timmins <scott@powershift.tv>
2016-09-15 09:48:39 +0100

    feat: add extended tests

--- tests/features/login.js
+++ tests/features/login.js
@@ -2,7 +2,7 @@ var config = require('../features-config');
 
 describe('Login Page', function() {
   before(function() {
-    browser.url(config.baseUrl + '/login');
+    browser.url(config.baseUrl);
   });
 
   describe('Check required elements', function() {
@@ -29,6 +29,82 @@ describe('Login Page', function() {
 
     it('the login form should have a forgotten password link', function() {
       browser.isExisting('#login a.forgotten-password-link').should.be.true;
+    });
+  });
+
+  describe.skip('Check client-side validation', function() {
+    beforeEach(function() {
+      browser.refresh();
+      browser.waitForExist('#login');
+    });
+
+    it('should display an error if no email is supplied', function() {
+      browser.setValue('#login #email', '');
+      browser.setValue('#login #password', 'password');
+      browser.leftClick('#login .login-button');
+      browser.waitForExist('#login .email-error');
+      browser.isExisting('#login .email-error').should.be.true;
+    });
+
+    it('should display an error if an invalid email is supplied', function() {
+      browser.setValue('#login #email', 'bob');
+      browser.setValue('#login #password', 'password');
+      browser.leftClick('#login .login-button');
+      browser.waitForExist('#login .email-error');
+      browser.isExisting('#login .email-error').should.be.true;
+    });
+
+    it('should display an error if no password is supplied', function() {
+      browser.setValue('#login #email', 'valid-email@example.com');
+      browser.setValue('#login #password', '');
+      browser.leftClick('#login .login-button');
+      browser.waitForExist('#login .password-error');
+      browser.isExisting('#login .password-error').should.be.true;
+    });
+  });
+
+  describe.skip('Check server-side validation', function() {
+    beforeEach(function() {
+      browser.refresh();
+      browser.waitForExist('#login');
+    });
+
+    it('should display an error if an invalid login is supplied', function() {
+      browser.setValue('#login #email', 'invalid-username@example.com');
+      browser.setValue('#login #password', 'invalid-password');
+      browser.leftClick('#login .login-button');
+      browser.waitForExist('#login .invalid-login-error');
+      browser.isExisting('#login .invalid-login-error').should.be.true;
+    });
+
+    it('should display an error if an invalid email is supplied', function() {
+      browser.setValue('#login #email', 'invalid-username@example.com');
+      browser.setValue('#login #password', 'valid-password');
+      browser.leftClick('#login .login-button');
+      browser.waitForExist('#login .invalid-login-error');
+      browser.isExisting('#login .invalid-login-error').should.be.true;
+    });
+
+    it('should display an error if an invalid password is supplied', function() {
+      browser.setValue('#login #email', 'valid-username@example.com');
+      browser.setValue('#login #password', 'invalid-password');
+      browser.leftClick('#login .login-button');
+      browser.waitForExist('#login .invalid-login-error');
+      browser.isExisting('#login .invalid-login-error').should.be.true;
+    });
+
+    it('should redirect if a valid login is supplied', function() {
+      browser.setValue('#login #email', 'valid-username@example.com');
+      browser.setValue('#login #password', 'valid-password');
+      browser.leftClick('#login .login-button');
+      browser.waitUntil(
+        function() {
+          return browser.url === config.baseUrl + '/select-customer';
+        },
+        5000,
+        100
+      );
+      browser.url().should.equal(config.baseUrl + '/select-customer');
     });
   });
 });
